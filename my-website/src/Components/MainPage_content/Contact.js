fc3c6298c57d4c616917b438bd637af564960cf1
BaranKazan <Baran.kazan@hotmail.com>
2020-10-28 19:26:40 +0100

    Contact us works now

--- my-website/src/Components/MainPage_content/Contact.js
+++ my-website/src/Components/MainPage_content/Contact.js
@@ -24,7 +24,7 @@ class Contact extends Component {
                 <div className="section-wrapper block">
                     <div className="content-1300">
                         <div className="row">
-                            <h2 className="entry-title section-title">Get in touch</h2>
+                            <h2 className="entry-title section-title">Contact</h2>
                             <div className="one-half width-40">
 
                                 <p className="section-info">
@@ -38,10 +38,10 @@ class Contact extends Component {
                                 <div className="contact-form" onSubmit={this.buttonClicked}>
                                     <form>
                                         <p>
-                                            <input id="name" type="text" name="name" placeholder="Name*" onChange={this.changeName} />
+                                            <input id="name" type="text" name="name" placeholder="Full name*" onChange={this.changeName} />
                                         </p>
                                         <p>
-                                            <input id="contact-email" type="email" name="email" placeholder="Email*" onChange={this.changeEmail} />
+                                            <input id="email" type="email" name="email" placeholder="Email*" onChange={this.changeEmail} />
                                         </p>
                                         <p>
                                             <input id="subject" type="text" name="subject" placeholder="Subject" />
@@ -69,17 +69,17 @@ class Contact extends Component {
             if (this.state.validName === null || this.state.validName === false) {
                 pass = false
                 this.setState({ validName: false })
-                //document.getElementById("firstName").style.border = "2px groove red";
+                document.getElementById("name").style.borderBottom = "2px solid red"
             }
             if (this.state.validEmail === null || this.state.validEmail === false) {
                 pass = false
                 this.setState({ validEmail: false })
-                //document.getElementById("lastName").style.border = "2px groove red";
+                document.getElementById("name").style.borderBottom = "2px solid red"
             }
             if (this.state.validMessage === null || this.state.validMessage === false) {
                 pass = false
                 this.setState({ validMessage: false })
-                //document.getElementById("companyName").style.border = "2px groove red";
+                document.getElementById("name").style.borderBottom = "2px solid red"
             }
             if (pass) {
                 this.setState({ eventTarget: e.target })
@@ -111,10 +111,10 @@ class Contact extends Component {
     changeName = (e) => {
         if (!(e.target.value === "")) {
             this.setState({ validName: true })
-            //document.getElementById("firstName").style.border = "2px groove green";
+            document.getElementById("name").style.borderBottom = "2px solid rgb(76 181 23)"
         } else {
             this.setState({ validName: false })
-            //document.getElementById("firstName").style.border = "2px groove red";
+            document.getElementById("name").style.borderBottom = "2px solid red"
         }
     }
 
@@ -122,20 +122,20 @@ class Contact extends Component {
         const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         if (regex.test(e.target.value)) {
             this.setState({ validEmail: true });
-            //document.getElementById("email").style.border = "2px groove green";
+            document.getElementById("email").style.borderBottom = "2px solid rgb(76 181 23)"
         } else {
             this.setState({ validEmail: false });
-            //document.getElementById("email").style.border = "2px groove red";
+            document.getElementById("email").style.borderBottom = "2px solid red"
         }
     }
 
     changeMessage = (e) => {
         if (!(e.target.value === "")) {
             this.setState({ validMessage: true })
-            //document.getElementById("message").style.border = "2px groove green";
+            document.getElementById("message").style.borderBottom = "2px solid rgb(76 181 23)"
         } else {
             this.setState({ validMessage: false })
-            //document.getElementById("message").style.border = "2px groove red";
+            document.getElementById("message").style.borderBottom = "2px solid red"
         }
     }
 
