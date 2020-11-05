786588afc9fb61000b960598dc5bc938569f6af5
BaranKazan <Baran.kazan@hotmail.com>
2020-11-05 20:00:01 +0100

    Update Header.js

--- my-website/src/Components/MainPage_content/Header.js
+++ my-website/src/Components/MainPage_content/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
             <div id="home" className="section full-width-section">
                 <div className="section-wrapper block">
                     <div className="home-left-part">
-                        <p className="site-des">What's up. My name is</p>
+                        <p className="site-des">Hello. My name is</p>
                         <h1 className="entry-title">Baran Kazan</h1>
                         <p className="site-info">
                             I'm an educated computer engineer and, my ambition is to be the best full-stack developer as well as I can .
