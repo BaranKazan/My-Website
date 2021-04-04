674db790af751edfdacd59506933337fb12a55c8
BaranKazan <baran.kazan@hotmail.com>
2021-04-04 15:55:22 +0200

    Chnaged CV

--- src/Components/MainPage_content/Service.js
+++ src/Components/MainPage_content/Service.js
@@ -60,7 +60,7 @@ class Service extends Component {
                                         </p>
 
                                 <div className="button-group-wrapper">
-                                    <a className="button" href="https://drive.google.com/file/d/1QzHtnCA9GEo3dRCSoBF4BB8nP4YqSM9j/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV</a>
+                                    <a className="button" href="https://drive.google.com/file/d/1jsQJ9BiGFkBlMEnh0gtW0E_mlXx3bLWs/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV</a>
                                     <a href="#portfolio" className="button">Check My Portfolio</a>
                                 </div>
                             </div>
